b0449aa6f39c3f1a0ee588911c4e6a1108b0a69b
lookymlive <lookymlive@gmail.com>
2024-10-07 10:47:56 -0300

    Dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,8 @@
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return <div className="container" style={{ padding: "10px" }}>
+    <h1 className="text-2xl">HomePage</h1>
+  </div>
+  ;
+
 };
 export default HomePage;
